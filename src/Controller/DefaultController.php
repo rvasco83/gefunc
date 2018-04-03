d725ec491a5e0d6886b9c7c0405225ba77852130
Rodrigo Vasco <r.vasco@hotmail.com>
2018-04-03 13:33:18 -0300

    Alteração do Layout

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     * @Template("default/index.html.twig")
+     */
+    public function index()
+    {
+        return [];
+    }
+}
