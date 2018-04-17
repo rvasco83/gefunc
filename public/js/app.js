d850ae904c59f3795c8a2efde9fcd032f7ce1a0d
Rodrigo Vasco <r.vasco@hotmail.com>
2018-04-17 11:33:16 -0300

    Criação do JavaScript

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,35 @@
 $('#funcionario_cargo').on('change', function () {
     var valor = $(this).val();
+    var status = $('#funcionario_status').val();
+
+    controlaExibicaoCamposFuncionario(valor, status);
+
+});
+
+function controlaExibicaoCamposFuncionario(cargo, status) {
+    var funcionarioExonerado = false;
+
+    if (status == 'E'){
+        funcionarioExonerado = true;
+    }
+
+    $('#funcionario_salario_base').prop('readonly', funcionarioExonerado);
+    $('#funcionario_desconto').prop('readonly', funcionarioExonerado);
+    $('#funcionario_gratificacao').prop('readonly', funcionarioExonerado);
+
+    $('#funcionario_data_exoneracao').prop('disabled', !funcionarioExonerado);
 
     $('#funcionario_gratificacao').prop('disabled', true);
 
-    if (valor == 'E') {
+    if (cargo == 'E') {
         $('#funcionario_gratificacao').prop('disabled', false);
     }
-});
+}
 
 $('#funcionario_status').on('change', function () {
-    var valor = $(this).val();
+    var status = $(this).val();
+    var cargo = $('#funcionario_cargo').val();
 
-    $('#funcionario_data_exoneracao .form-control').prop('disabled', true);
+    controlaExibicaoCamposFuncionario(cargo, status);
 
-    if (valor == 'E') {
-        $('#funcionario_data_exoneracao .form-control').prop('disabled', false);
-    }
 });
